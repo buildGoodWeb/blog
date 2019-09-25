e8aeac8dfe9e23f7cb621757eec2644614e94a2c
王开朗 <wangkailang992@gmail.com>
2019-09-25 11:37:00 +0800

    add sass theme blog

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,42 +1,63 @@
 import React from "react"
 import { Link } from "gatsby"
 import './style.scss';
+import Toggle from '../Toggle';
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children, isPost } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
+const isClient = typeof window === 'object';
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1>
-          <Link to={`/`} >
-            {title}
-          </Link>
-        </h1>
-      )
+const Layout = props => {
+  const { location, title, children, isPost } = props;
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  const [cls, setCls] = React.useState(() => {
+    if (isClient) {
+      return window.localStorage.getItem('theme') || 'light';
     } else {
-      header = (
-        <h3>
-          <Link to={`/`}>
-            {title}
-          </Link>
-        </h3>
-      )
+      return 'light';
     }
-    return (
+  });
+  const toggle = React.useCallback(() => {
+    const theme = cls === 'light' ? 'dark' : 'light';
+    setCls(theme);
+    isClient && window.localStorage.setItem('theme', theme);
+  }, [cls, setCls]);
+
+  if (location.pathname === rootPath) {
+    header = (
+      <h1>
+        <Link to={`/`} >
+          {title}
+        </Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <h3>
+        <Link to={`/`}>
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+  return (
+    <div className={`theme-${cls}`}>
       <div className={`layout ${isPost ? 'post' : ''}`}>
-        <header>{header}</header>
+        <header>
+          {header}
+          <div onClick={toggle} className="toggle">
+            {cls === 'light' ? <Toggle.Dark /> : <Toggle.Light />}
+          </div>
+        </header>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Github
-          {` `}
+        {` `}
           <a href="https://github.com/buildGoodWeb/blog">Blog</a>
         </footer>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Layout
